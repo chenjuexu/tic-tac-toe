01adf5fbd75ee12732545ee6557607a4f9eb2af2
unknown <chenjuexu007@gmail.com>
2022-02-21 19:37:41 -0500

    first branch

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,21 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+      presets: ["babel-preset-expo"],
+      plugins: [
+          [
+              "module-resolver",
+              {
+                  alias: {
+                      "@screens": "./src/screens",
+                      "@Components": "./src/components",
+                      "@utils": "./src/utils",
+                      "@contexts": "./src/contexts",
+                      "@config": "./src/config",
+                      "@assets": "./assets"
+                  }
+              }
+          ]
+      ]
   };
 };
